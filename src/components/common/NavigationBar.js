bcc8e80682cda59184805d867d8d36aebf4818d6
Mate Antičević <mate@anticevic.net>
2017-06-29 23:34:08 +0200

    PIW-2 Logout action

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -4,8 +4,14 @@ import Nav from 'react-bootstrap/lib/Nav';
 import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 import { Link, IndexLink } from 'react-router';
+import { browserHistory } from 'react-router';
 
 const NavigationBar = () => {
+
+  function logout(){
+    localStorage.removeItem("token");
+    browserHistory.push("/login");
+  }
 
   return (
          <Navbar fixedTop={true}>
@@ -15,12 +21,17 @@ const NavigationBar = () => {
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>
-            <NavDropdown eventKey={3} title="Finance" id="basic-nav-dropdown">
+            <NavDropdown eventKey={3} title="Finance">
               <MenuItem><Link to="/expenses">Expenses</Link></MenuItem>
               <MenuItem eventKey={3.2}>Incomes</MenuItem>
             </NavDropdown>
-            <NavDropdown eventKey={3} title="Travel" id="basic-nav-dropdown">
+            <NavDropdown eventKey={3} title="Travel">
               <MenuItem><Link to="/trips">Trips</Link></MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavDropdown eventKey={4} title="Mate Antičević">
+              <MenuItem><Link onClick={logout}>Logout</Link></MenuItem>
             </NavDropdown>
           </Nav>
          </Navbar>
