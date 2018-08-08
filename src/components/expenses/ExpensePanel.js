ca449377c27e5e246b38340acde708a20a4e6c22
Mate Antičević <mate@anticevic.net>
2018-08-08 21:07:18 +0200

    Pagination moved to component

--- src/components/expenses/ExpensePanel.js
+++ src/components/expenses/ExpensePanel.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Row, Col, Pagination, Panel, Button } from 'react-bootstrap/lib';
+import { Row, Col, Panel, Button } from 'react-bootstrap/lib';
 import FontAwesome from 'react-fontawesome';
 import _ from 'lodash';
 
 import ExpenseTable from './ExpenseTable';
+import Pagination from '../common/Pagination';
 
 const ExpensePanel = (props) => {
   
@@ -11,13 +12,9 @@ const ExpensePanel = (props) => {
 
   const expenses = props.serverPaging ? props.expenses.items : props.expenses.items.slice((props.page - 1) * props.pageSize, props.page * props.pageSize);
 
-  //const totalItems = props.serverPaging ? props.expenses.count : props.expenses.items.length;
+  const totalItems = props.serverPaging ? props.expenses.count : props.expenses.items.length;
 
   const expenseTable = expenses && expenses.length > 0 ? <ExpenseTable expenses={expenses} onEdit={props.onEdit} onUnlink={props.onUnlink} /> : <h2>No data</h2>;
-
-  const startFromPage = props.page > 2 ? props.page - 2 : 1;
-
-  const paginationItems = _.range(startFromPage, startFromPage + 5).map(item => <Pagination.Item active={item === props.page} onClick={page => props.onPageChange({ page: item })}>{item}</Pagination.Item>);
 
   return (
     <Panel>
@@ -41,13 +38,7 @@ const ExpensePanel = (props) => {
         </Row>
         <Row>
           <Col lg={12}>
-            {/*             <Pagination prev next first last ellipsis boundaryLinks items={Math.ceil(totalItems / props.pageSize)}
-              maxButtons={5}
-              activePage={props.page}
-              onSelect={page => props.onPageChange({ page: page })} /> */}
-            <Pagination>
-              {paginationItems}
-            </Pagination>
+            <Pagination page={props.page} pages={Math.ceil(totalItems / props.pageSize)} onPageChange={props.onPageChange} />
           </Col>
         </Row>
       </Panel.Body>
