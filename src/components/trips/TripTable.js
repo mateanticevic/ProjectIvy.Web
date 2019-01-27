613c7086815676d145bfc2b05a864ffd72473412
Mate Antičević <mate@anticevic.net>
2019-01-27 09:39:37 +0100

    Remove trip table headers

--- src/components/trips/TripTable.js
+++ src/components/trips/TripTable.js
@@ -4,17 +4,10 @@ import Table from 'react-bootstrap/lib/Table';
 
 const TripTable = (props) => {
   return (
-    <Table responsive >
-        <thead>
-            <tr>
-            <th>Year</th>
-            <th>Countries</th>
-            <th>Name</th>
-            <th>Duration</th>
-            <th/>
-            </tr>
-            {props.children}
-        </thead>
+    <Table responsive>
+      <tbody>
+        {props.children}
+      </tbody>
     </Table>
   );
 };
