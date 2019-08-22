beb31a1e44d024d35266edf652793c651a533755
Mate Antičević <mate@anticevic.net>
2019-08-22 23:06:30 +0200

    Trackings page multiple days enabled

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,4 +1,4 @@
-import { default as React, Children } from "react";
+import { default as React } from "react";
 import GoogleMaps from './GoogleMaps';
 
 type Props = {
--- src/pages/tracking/index.tsx
+++ src/pages/tracking/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Col, Panel, ControlLabel, Row } from 'react-bootstrap/lib';
+import { Grid, Col, Panel, ControlLabel, Row, Table } from 'react-bootstrap/lib';
 import { Polyline } from "react-google-maps";
 import Datetime from 'react-datetime';
 import moment from 'moment';
@@ -10,6 +10,16 @@ import * as trackingApi from '../../api/main/tracking';
 import { Page } from '../Page';
 
 class TrackingPage extends Page<{}, {}> {
+
+    colors = [
+        '#000000',
+        '#ff0000',
+        '#00a6ff',
+        '#166e00',
+        '#ff00f2',
+        '#00fbff',
+        '#ffff00',
+    ];
 
     state = {
         filters: {
@@ -32,11 +42,18 @@ class TrackingPage extends Page<{}, {}> {
 
         this.setState({ filters: filters });
         const nextDay = moment(filters.day).add(1, 'days').format("YYYY-MM-DD");
-        trackingApi.get({ from: filters.day, to: nextDay }).then(trackings => this.setState({ trackings: trackings }));
+        trackingApi.get({ from: filters.day, to: nextDay }).then(trackings => this.setState({ trackings: [...this.state.trackings, { day: filters.day, trackings }] }));
+    }
+
+    @boundMethod
+    renderTrackingDays(trackings) {
+        return trackings.map(tracking => <tr>
+            <td>{tracking.day}</td>
+        </tr>);
     }
 
     render() {
-        const filters = this.state;
+        const { filters, trackings } = this.state;
 
         return (
             <Grid>
@@ -49,13 +66,23 @@ class TrackingPage extends Page<{}, {}> {
                                 <Datetime dateFormat="YYYY-MM-DD" timeFormat={false} value={filters.day} onChange={x => this.onFiltersChanged({ day: x.format("YYYY-MM-DD") })} />
                             </Panel.Body>
                         </Panel>
+                        <Panel>
+                            <Panel.Heading>Days</Panel.Heading>
+                            <Panel.Body>
+                                <Table>
+                                    <tbody>
+                                        {this.renderTrackingDays(trackings)}
+                                    </tbody>
+                                </Table>
+                            </Panel.Body>
+                        </Panel>
                     </Col>
                     <Col lg={9}>
                         <Panel>
                             <Panel.Heading>Map</Panel.Heading>
                             <Panel.Body className="padding-0 panel-medium">
                                 <Map defaultZoom={12} defaultCenter={{ lat: 45.794441, lng: 15.928380 }}>
-                                    <Polyline path={this.state.trackings} />
+                                    {trackings.map((tracking, i) => <Polyline path={tracking.trackings} options={{strokeColor: this.colors[i % this.colors.length]}} />)}
                                 </Map>
                             </Panel.Body>
                         </Panel>
