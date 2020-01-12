049088f64596f782ef9892b42f1f309ed60578cb
Mate Antičević <mate@anticevic.net>
2020-01-12 13:48:49 +0100

    Filters to query fixed

--- src/pages/Page.ts
+++ src/pages/Page.ts
@@ -5,7 +5,7 @@ import * as urlHelper from '../utils/urlHelper';
 export class Page<TProps, TState> extends React.Component<TProps, TState> {
 
     public pushHistoryState(filters) {
-        window.history.pushState(null, null, window.location.pathname + urlHelper.jsonToQueryString(filters));
+        window.history.pushState(null, null, window.location.pathname + '?' + urlHelper.jsonToQueryString(filters));
     }
 
     public resolveFilters(filters, filterValue) {
