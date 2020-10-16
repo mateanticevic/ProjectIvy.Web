eef2183a9ced57303e4a30a852cab021815a6f35
Mate Antičević <mate.anticevic@infobip.com>
2020-10-16 23:33:00 +0200

    tslint replaced with eslint

--- src/utils/urlHelper.ts
+++ src/utils/urlHelper.ts
@@ -1,41 +1,41 @@
 export function jsonToQueryString(queryObj, nesting = '') {
-    const queryString = '';
+  const queryString = '';
 
-    const pairs = Object.entries(queryObj).map(([key, val]) => {
-      // Handle a second base case where the value to encode is an array
-      if (Array.isArray(val)) {
-        return val
-          .map(subVal => [nesting + key, subVal].map(escape).join('='))
-          .join('&');
-      } else if (typeof val === 'object') {
-        return jsonToQueryString(val, nesting + `${key}.`);
-      } else {
-        return [nesting + key, val].map(escape).join('=');
-      }
-    });
-    return pairs.filter(x => x).join('&');
-  }
+  const pairs = Object.entries(queryObj).map(([key, val]) => {
+    // Handle a second base case where the value to encode is an array
+    if (Array.isArray(val)) {
+      return val
+        .map(subVal => [nesting + key, subVal].map(escape).join('='))
+        .join('&');
+    } else if (typeof val === 'object') {
+      return jsonToQueryString(val, nesting + `${key}.`);
+    } else {
+      return [nesting + key, val].map(escape).join('=');
+    }
+  });
+  return pairs.filter(x => x).join('&');
+}
 
 export function queryStringToJson(queryString: string) {
-    const search = queryString.substring(1);
-    return search ? JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-        function(key, value) { return key === '' ? value : parseValue(value); }) : {};
+  const search = queryString.substring(1);
+  return search ? JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+    function (key, value) { return key === '' ? value : parseValue(value); }) : {};
 }
 
 function parseValue(value) {
-    const decoded = decodeURIComponent(value);
+  const decoded = decodeURIComponent(value);
 
-    return isNaN(decoded) || decoded[0] === '0' ? decoded : parseInt(value);
+  return isNaN(decoded) || decoded[0] === '0' ? decoded : parseInt(value);
 }
 
 function objectToArray(parent: string, json: any) {
-    return Object.keys(json).map(function(key) {
-        return parent + '=' +
-            encodeURIComponent(json[key]);
-    }).join('&');
+  return Object.keys(json).map(function (key) {
+    return parent + '=' +
+      encodeURIComponent(json[key]);
+  }).join('&');
 }
 
 export function getResourceFromUrl() {
-    const resourceIndex = window.location.pathname.lastIndexOf('/') + 1;
-    return window.location.pathname.substring(resourceIndex);
+  const resourceIndex = window.location.pathname.lastIndexOf('/') + 1;
+  return window.location.pathname.substring(resourceIndex);
 }
