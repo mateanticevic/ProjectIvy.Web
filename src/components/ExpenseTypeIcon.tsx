f650bbfdf609406762384684b0fff994b53234f4
Mate Antičević <mate@anticevic.net>
2021-03-29 21:56:49 +0200

    Expense type icons updated

--- src/components/ExpenseTypeIcon.tsx
+++ src/components/ExpenseTypeIcon.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import { BiDrink, BiRestaurant, BiTaxi } from 'react-icons/bi';
 import { CgGames } from 'react-icons/cg';
 import { MdBatteryFull, MdFlight } from 'react-icons/md';
-import { FaParking, FaTv, FaShoppingBasket, FaCoffee, FaGasPump } from 'react-icons/fa';
-import { GiCarWheel, GiFoodTruck } from 'react-icons/gi';
+import { FaParking, FaTv, FaShoppingBasket, FaCoffee, FaGasPump, FaWarehouse, FaUsb, FaCar, FaGlasses, FaMobile, FaRoad, FaBolt, FaFire, FaBuilding } from 'react-icons/fa';
+import { GiCarWheel, GiFoodTruck, GiSonicShoes, GiHomeGarage } from 'react-icons/gi';
 import { GoPackage } from 'react-icons/go';
 
 const icons = {
     ['alcoholic-beverages']: <BiDrink />,
     ['batteries']: <MdBatteryFull />,
+    ['building']: <FaBuilding />,
+    ['cables']: <FaUsb />,
+    ['car-rental']: <FaCar />,
     ['coffee']: <FaCoffee />,
+    ['contact-lenses']: <FaGlasses />,
     ['car-maintenance']: <GiCarWheel />,
     ['games']: <CgGames />,
+    ['gas']: <FaFire />,
     ['delivery']: <GoPackage />,
     ['drinks']: <BiDrink />,
+    ['electricity']: <FaBolt />,
+    ['house-repair']: <FaWarehouse />,
+    ['house-supplies']: <FaWarehouse />,
     ['flight']: <MdFlight />,
     ['fuel']: <FaGasPump />,
     ['food-delivery']: <GiFoodTruck />,
+    ['garage']: <GiHomeGarage />,
     ['groceries']: <FaShoppingBasket />,
+    ['mobile-subscription']: <FaMobile />,
     ['parking']: <FaParking />,
+    ['radiotelevision']: <FaTv />,
     ['restaurant']: <BiRestaurant />,
+    ['shoes']: <GiSonicShoes />,
     ['taxi']: <BiTaxi />,
+    ['toll']: <FaRoad />,
     ['tv']: <FaTv />,
 };
 
