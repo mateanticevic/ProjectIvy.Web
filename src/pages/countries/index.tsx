adc933e817bbefbe870905d27ba6261310e1956b
Mate Anticevic <mate@anticevic.net>
2022-10-22 23:14:29 +0200

    Countries - visited filter added

--- src/pages/countries/index.tsx
+++ src/pages/countries/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Card, Container, Row } from 'react-bootstrap';
+import { ButtonGroup, Card, Col, Container, Row, ToggleButton } from 'react-bootstrap';
 import classNames from 'classnames';
 
 import api from 'api/main';
 import { Page } from 'pages/page';
 import { Country } from 'types/common';
 
+enum CountryVisited {
+    All,
+    Yes,
+    No,
+}
+
+interface Filters {
+    visited: CountryVisited,
+}
 
 interface State {
     countries: Country[],
+    filters: Filters,
     visitedCountries: Country[],
 }
 
@@ -16,6 +26,9 @@ class CountriesPage extends Page<{}, State> {
 
     state: State = {
         countries: [],
+        filters: {
+            visited: CountryVisited.All,
+        },
         visitedCountries: [],
     }
 
@@ -25,21 +38,61 @@ class CountriesPage extends Page<{}, State> {
     }
 
     render() {
-        const { countries, visitedCountries } = this.state;
+        const { countries, filters, visitedCountries } = this.state;
+        const filteredCountries = filters.visited === CountryVisited.All ?
+            countries
+            : filters.visited === CountryVisited.Yes ? visitedCountries : countries.filter(c => !visitedCountries.map(x => x.id).includes(c.id));
+
+        const visitedRadios = [
+            { name: 'All', value: CountryVisited.All },
+            { name: 'Yes', value: CountryVisited.Yes },
+            { name: 'No', value: CountryVisited.No },
+        ];
+
         return (
             <Container>
                 <Row>
-                    {countries.map(country =>
+                    <Col lg={3}>
                         <Card>
-                            <Card.Body className={classNames('country-item', { 'country-item-not-visited': !visitedCountries.map(x => x.id).includes(country.id) })}>
-                                <span className={`flag-icon flag-icon-${country.id?.toLowerCase()} country-flag`} />
-                                <h2>{country.name}</h2>
+                            <Card.Header>Filters</Card.Header>
+                            <Card.Body>
+                                <ButtonGroup>
+                                    {visitedRadios.map(radio =>
+                                        <ToggleButton
+                                            type="radio"
+                                            checked={filters.visited === radio.value}
+                                            value={radio.value}
+                                            onClick={() => this.onFiltersChanged({ visited: radio.value })}
+                                        >
+                                            {radio.name}
+                                        </ToggleButton>
+                                    )}
+                                </ButtonGroup>
                             </Card.Body>
                         </Card>
-                    )}
+                    </Col>
+                    <Col lg={6}>
+                        {filteredCountries.sort((a, b) => a.name.localeCompare(b.name)).map(country =>
+                            <Card>
+                                <Card.Body className={classNames('country-item', { 'country-item-not-visited': !visitedCountries.map(x => x.id).includes(country.id) })}>
+                                    <span className={`flag-icon flag-icon-${country.id?.toLowerCase()} country-flag`} />
+                                    <h2>{country.name}</h2>
+                                </Card.Body>
+                            </Card>
+                        )}
+                    </Col>
                 </Row>
-            </Container>
+            </Container >
         );
+    }
+
+    onFiltersChanged = (changed: Partial<Filters>) => {
+        this.setState({
+            filters: {
+                ...this.state.filters,
+                ...changed,
+            }
+        });
     }
 }
 
