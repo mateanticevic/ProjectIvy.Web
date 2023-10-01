83e4daf02b806ddd9ef6c29640ac27d654dd62ae
Mate Anticevic <mate@anticevic.net>
2023-10-01 19:26:39 +0200

    new expense type icons added

--- src/components/expense-type-icon.tsx
+++ src/components/expense-type-icon.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { BiDrink, BiRestaurant, BiTaxi, BiDollar } from 'react-icons/bi';
+import { BiDrink, BiRestaurant, BiTaxi, BiDollar, BiCameraMovie, BiSolidTShirt } from 'react-icons/bi';
+import { BsMotherboard } from 'react-icons/bs';
 import { CgGames } from 'react-icons/cg';
-import { MdBatteryFull, MdFlight } from 'react-icons/md';
-import { FaParking, FaTv, FaShoppingBasket, FaCoffee, FaGasPump, FaWarehouse, FaUsb, FaCar, FaGlasses, FaMobile, FaRoad, FaBolt, FaFire, FaBuilding, FaTooth } from 'react-icons/fa';
+import { FaHamburger, FaMemory } from 'react-icons/fa';
+import { FiCpu, FiPower } from 'react-icons/fi';
+import { FcInspection } from 'react-icons/fc';
+import { FaFerry } from 'react-icons/fa6';
+import { GiPresent, GiClothes } from 'react-icons/gi';
+import { MdBatteryFull, MdFlight, MdCarRepair } from 'react-icons/md';
+import { FaCouch, FaParking, FaTv, FaShoppingBasket, FaCoffee, FaGasPump, FaWarehouse, FaUsb, FaCar, FaGlasses, FaMobile, FaRoad, FaBolt, FaFire, FaBuilding, FaTooth } from 'react-icons/fa';
 import { GiCarWheel, GiFoodTruck, GiSonicShoes, GiHomeGarage, GiDeliveryDrone } from 'react-icons/gi';
 import { GoPackage } from 'react-icons/go';
 
@@ -11,32 +17,47 @@ const icons = {
     ['batteries']: <MdBatteryFull />,
     ['building']: <FaBuilding />,
     ['cables']: <FaUsb />,
-    ['car-rental']: <FaCar />,
-    ['coffee']: <FaCoffee />,
-    ['contact-lenses']: <FaGlasses />,
+    ['car-inspection']: <FcInspection />,
     ['car-maintenance']: <GiCarWheel />,
     ['car-parts']: <GiCarWheel />,
-    ['games']: <CgGames />,
-    ['gas']: <FaFire />,
+    ['car-registration']: <FaCar />,
+    ['car-rental']: <FaCar />,
+    ['car-repair']: <MdCarRepair />,
+    ['cinema']: <BiCameraMovie />,
+    ['clothes-other']: <GiClothes />,
+    ['coffee']: <FaCoffee />,
+    ['contact-lenses']: <FaGlasses />,
     ['delivery']: <GoPackage />,
     ['dentist']: <FaTooth />,
     ['drinks']: <BiDrink />,
     ['drone']: <GiDeliveryDrone />,
     ['electricity']: <FaBolt />,
+    ['fast-food']: <FaHamburger />,
+    ['flight']: <MdFlight />,
+    ['ferry']: <FaFerry />,
+    ['furniture']: <FaCouch />,
+    ['food-delivery']: <GiFoodTruck />,
+    ['fuel']: <FaGasPump />,
+    ['games']: <CgGames />,
+    ['garage']: <GiHomeGarage />,
+    ['gas']: <FaFire />,
+    ['groceries']: <FaShoppingBasket />,
     ['house-repair']: <FaWarehouse />,
     ['house-supplies']: <FaWarehouse />,
-    ['flight']: <MdFlight />,
-    ['fuel']: <FaGasPump />,
-    ['food-delivery']: <GiFoodTruck />,
-    ['garage']: <GiHomeGarage />,
-    ['groceries']: <FaShoppingBasket />,
     ['mobile-subscription']: <FaMobile />,
+    ['motherboard']: <BsMotherboard />,
     ['parking']: <FaParking />,
+    ['power-supply']: <FiPower />,
+    ['present']: <GiPresent />,
+    ['processor']: <FiCpu />,
     ['radiotelevision']: <FaTv />,
+    ['ram-memory']: <FaMemory />,
     ['restaurant']: <BiRestaurant />,
     ['shoes']: <GiSonicShoes />,
+    ['shirt']: <BiSolidTShirt />,
     ['taxi']: <BiTaxi />,
     ['toll']: <FaRoad />,
+    ['traffic-fine']: <FaCar />,
     ['tv']: <FaTv />,
 };
 
