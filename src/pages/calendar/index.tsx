d0dede25adad1de56a2fc1bbb396b9485670af0c
Mate Anticevic <mate@anticevic.net>
2024-03-17 10:22:48 +0100

    Calendar - month picker added

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,38 +1,45 @@
 import React from 'react';
+import Datetime from 'react-datetime';
 
 import { Page } from 'pages/page';
 import { Container } from 'react-bootstrap';
 import { components } from 'types/ivy-types';
 import api from 'api/main';
 import { CalendarDay } from './calendar-day';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 
 type CalendarSection = components['schemas']['CalendarSection'];
 
 interface State {
     calendarSection?: CalendarSection;
+    startDay: Moment;
 }
 
 class CalendarPage extends Page<unknown, State> {
 
     state: State = {
+        startDay: moment().startOf('month')
     }
 
     componentDidMount() {
-        api.calendar.getSection('2024-2-1', '2024-3-1')
-            .then(calendarSection => {
-                this.setState({ calendarSection });
-            });
+        const { startDay } = this.state;
+        this.onMonthChanged(startDay);
     }
 
     render() {
         const { calendarSection } = this.state;
         return (
             <Container>
+                <Datetime
+                    inputProps={{ readOnly: true }}
+                    dateFormat="MMMM YYYY"
+                    timeFormat={false}
+                    onChange={month => this.onMonthChanged(month as Moment)}
+                />
                 <div className="calendar-container">
                     {calendarSection?.days?.reverse().map((day, i) =>
                         <CalendarDay
-                            key={moment(day.date).format('YYYY-MM-dd')}
+                            key={moment(day.date).format('YYYY-MM-DD')}
                             day={day}
                             offset={i === 0 ? moment(day.date).weekday() + 1 : 0}
                             onWorkDayTypeChange={workDayTypeId => this.onWorkDayTypeChange(moment(day.date).format('YYYY-MM-DD'), workDayTypeId)}
@@ -41,6 +48,16 @@ class CalendarPage extends Page<unknown, State> {
                 </div>
             </Container >
         );
+    }
+
+    onMonthChanged = (month: Moment) => {
+        api.calendar.getSection(month.startOf('month').format('YYYY-MM-DD'), month.clone().endOf('month').format('YYYY-MM-DD'))
+            .then(calendarSection => {
+                this.setState({
+                    calendarSection,
+                    startDay: month.startOf('month')
+                });
+            });
     }
 
     onWorkDayTypeChange = (date: string, workDayTypeId: string) => {
