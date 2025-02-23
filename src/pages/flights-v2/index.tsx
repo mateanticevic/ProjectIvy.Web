de8c7d2875f339dee0c7652ccf03bd3bf658e89a
Mate Anticevic <mate@anticevic.net>
2025-02-23 09:18:47 +0100

    flights v2 page converted to functional component

--- src/pages/flights-v2/index.tsx
+++ src/pages/flights-v2/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import InfiniteScroll from 'react-infinite-scroll-component';
-
 import api from 'api/main';
-import { Page } from 'pages/page';
 import FlightItem from './flight-item';
 import { components } from 'types/ivy-types';
 import FlightModal from './flight-modal';
@@ -13,19 +11,8 @@ type Flight = components['schemas']['Flight'];
 type FlightBinding = components['schemas']['FlightBinding'];
 
 interface Filter {
-    page: number,
-    pageSize: number,
-}
-
-interface State {
-    count: number,
-    countBy: CountByFlights,
-    countByData: any,
-    filter: Filter,
-    flight: Flight,
-    flightBinding: FlightBinding,
-    flights: Flight[],
-    isModalOpen: boolean,
+    page: number;
+    pageSize: number;
 }
 
 enum CountByFlights {
@@ -43,140 +30,117 @@ const countApiMapping = {
     [CountByFlights.Airport]: api.flight.getCountByAirport,
 };
 
-class FlightsV2Page extends Page<unknown, State> {
+const FlightsV2Page: React.FC = () => {
+    const [count, setCount] = useState(0);
+    const [countBy, setCountBy] = useState(CountByFlights.Airline);
+    const [countByData, setCountByData] = useState<any>([]);
+    const [filter, setFilter] = useState<Filter>({ page: 1, pageSize: 200 });
+    const [flight, setFlight] = useState<Flight>({} as Flight);
+    const [flightBinding, setFlightBinding] = useState<FlightBinding>({} as FlightBinding);
+    const [flights, setFlights] = useState<Flight[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    state: State = {
-        count: 0,
-        countBy: CountByFlights.Airline,
-        filter: {
-            page: 1,
-            pageSize: 200,
-        },
-        flight: {},
-        flightBinding: {},
-        flights: [],
-        isModalOpen: false,
-    };
+    useEffect(() => {
+        fetchData();
+    }, [filter]);
 
-    componentDidMount() {
-        this.onFilterChanged();
-    }
-
-    render() {
-        const { count, countByData, filter, flight, flightBinding, flights, isModalOpen } = this.state;
-
-        return (
-            <Container>
-                <Row>
-                    <Col lg={3}>
-                        <Button onClick={() => this.setState({ isModalOpen: true })}>
-                            New
-                        </Button>
-                    </Col>
-                    <Col lg={6}>
-                        <InfiniteScroll
-                            dataLength={count}
-                            next={this.getNextsPage}
-                            hasMore={filter.page * filter.pageSize < count}
-                            loader={<h4>Loading...</h4>}
-                            endMessage="no more"
-
-                        >
-                            {flights.map(flight =>
-                                <FlightItem
-                                    key={flight.departure}
-                                    flight={flight}
-                                    onClick={() => this.onFlightClick(flight)}
-                                />
-                            )}
-                        </InfiniteScroll>
-                    </Col>
-                    <Col lg={3}>
-                        <DistributionCard
-                            countByOptions={countByOptions}
-                            data={countByData}
-                            name="Top 10"
-                            onGroupByChange={this.onCountByChange}
-                        />
-                    </Col>
-                </Row>
-                <FlightModal
-                    flight={flight}
-                    flightBinding={flightBinding}
-                    isOpen={isModalOpen}
-                    onChange={this.onFlightChanged}
-                    onClose={() => this.setState({ isModalOpen: false })}
-                    onSave={this.onFlightSave}
-                />
-            </Container>
-        );
-    }
-
-    getNextPage = () => {
-        this.onFilterChanged({
-            page: this.state.filter.page + 1,
-        });
-    };
-
-    onFlightClick = (flight: Flight) => {
-        this.setState({
-            flight,
-            flightBinding: {
-                airlineId: flight.airline?.id,
-                arrival: flight.arrival,
-                arrivalLocal: flight.arrivalLocal,
-                departure: flight.departure,
-                departureLocal: flight.departureLocal,
-                number: flight.number,
-                originId: flight.origin?.iata,
-                destinationId: flight.destination?.iata,
-            }
-        }, () => this.setState({ isModalOpen: true }));
-    };
-
-    onFlightChanged = (changed: Partial<Flight>) => {
-        this.setState({
-            flightBinding: {
-                ...this.state.flightBinding,
-                ...changed,
-            }
-        });
-    };
-
-    onFlightSave = () => {
-
-        if (this.state.flight.id) {
-            api.flight.put(this.state.flight.id, this.state.flightBinding)
-                .then(() => this.setState({ isModalOpen: false }));
-        }
-        else {
-            api.flight.post(this.state.flightBinding)
-                .then(() => this.setState({ isModalOpen: false }));
-        }
-    };
-
-    onFilterChanged = (changedFilters?: Partial<Filter>) => {
-        const filter = {
-            ...this.state.filter,
-            ...changedFilters,
-        };
-        this.setState({ filter }, () => this.onCountByChange());
-
+    const fetchData = () => {
         api.flight.get(filter)
-            .then(result => this.setState({
-                count: result.count,
-                flights: this.state.flights.concat(result.items),
-            }));
+            .then(result => {
+                setCount(result.count);
+                setFlights(prevFlights => prevFlights.concat(result.items));
+            });
+
+        countApiMapping[countBy](filter)
+            .then(data => setCountByData(data.slice(0, 10)));
     };
 
-    onCountByChange = (countBy?: CountByFlights) => {
-        if (countBy) {
-            this.setState({ countBy });
+    const getNextPage = () => {
+        setFilter(prevFilter => ({ ...prevFilter, page: prevFilter.page + 1 }));
+    };
+
+    const onFlightClick = (flight: Flight) => {
+        setFlight(flight);
+        setFlightBinding({
+            airlineId: flight.airline?.id,
+            arrival: flight.arrival,
+            arrivalLocal: flight.arrivalLocal,
+            departure: flight.departure,
+            departureLocal: flight.departureLocal,
+            number: flight.number,
+            originId: flight.origin?.iata,
+            destinationId: flight.destination?.iata,
+        });
+        setIsModalOpen(true);
+    };
+
+    const onFlightChanged = (changed: Partial<Flight>) => {
+        setFlightBinding(prevBinding => ({ ...prevBinding, ...changed }));
+    };
+
+    const onFlightSave = () => {
+        if (flight.id) {
+            api.flight.put(flight.id, flightBinding)
+                .then(() => setIsModalOpen(false));
+        } else {
+            api.flight.post(flightBinding)
+                .then(() => setIsModalOpen(false));
+        }
+    };
+
+    const onCountByChange = (newCountBy?: CountByFlights) => {
+        if (newCountBy !== undefined) {
+            setCountBy(newCountBy);
         }
 
-        countApiMapping[countBy ?? this.state.countBy](this.state.filter)
-            .then(countByData => this.setState({ countByData: countByData.slice(0, 10) }));
+        countApiMapping[newCountBy ?? countBy](filter)
+            .then(data => setCountByData(data.slice(0, 10)));
     };
-}
+
+    return (
+        <Container>
+            <Row>
+                <Col lg={3}>
+                    <Button onClick={() => setIsModalOpen(true)}>
+                        New
+                    </Button>
+                </Col>
+                <Col lg={6}>
+                    <InfiniteScroll
+                        dataLength={count}
+                        next={getNextPage}
+                        hasMore={filter.page * filter.pageSize < count}
+                        loader={<h4>Loading...</h4>}
+                        endMessage="no more"
+                    >
+                        {flights.map(flight =>
+                            <FlightItem
+                                key={flight.departure}
+                                flight={flight}
+                                onClick={() => onFlightClick(flight)}
+                            />
+                        )}
+                    </InfiniteScroll>
+                </Col>
+                <Col lg={3}>
+                    <DistributionCard
+                        countByOptions={countByOptions}
+                        data={countByData}
+                        name="Top 10"
+                        onGroupByChange={onCountByChange}
+                    />
+                </Col>
+            </Row>
+            <FlightModal
+                flight={flight}
+                flightBinding={flightBinding}
+                isOpen={isModalOpen}
+                onChange={onFlightChanged}
+                onClose={() => setIsModalOpen(false)}
+                onSave={onFlightSave}
+            />
+        </Container>
+    );
+};
 
 export default FlightsV2Page;
