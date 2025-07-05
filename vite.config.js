9e56103fe60c1ec1ed9a3e2d490fa9156e711fca
Mate Anticevic <mate@anticevic.net>
2025-07-05 19:50:16 +0200

    npm update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    allowedHosts: ["local.anticevic.net"],
     host: '0.0.0.0',
     proxy: {
       '/api': {
