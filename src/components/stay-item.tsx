a7501ace8e5d6e80e3af642ffba13daec0852bdf
Mate Anticevic <mate@anticevic.net>
2025-11-18 21:45:20 +0100

    is booked indicator added to StayItem

--- src/components/stay-item.tsx
+++ src/components/stay-item.tsx
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import React from 'react';
-import { FaPencilAlt } from 'react-icons/fa';
+import { FaCheckCircle } from 'react-icons/fa';
 import { components } from 'types/ivy-types';
 
 type Stay = components['schemas']['Stay'];
@@ -13,14 +13,16 @@ interface StayItemProps {
 export const StayItem: React.FC<StayItemProps> = ({ stay, onEdit }) => {
     return (
         <div className="stay-item" onClick={() => onEdit(stay)} role="button" tabIndex={0}>
+            {stay.isBooked && (
+                <div className="stay-booked-indicator">
+                    <FaCheckCircle />
+                </div>
+            )}
             <div className="stay-content">
                 <strong>{stay.city?.name || 'Unnamed Stay'}</strong>
                 <div className="text-muted small">
                     {moment(stay.from).format('MMM DD')} - {moment(stay.to).diff(moment(stay.from), 'days')} nights
                 </div>
-            </div>
-            <div className="stay-edit-btn">
-                <FaPencilAlt />
             </div>
         </div>
     );
