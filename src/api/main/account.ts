ffa6d58ef2bc28b28bc754d08c836ece22d92557
Mate Anticevic <mate@anticevic.net>
2026-02-07 00:44:45 +0100

    Refactor account API methods to include type annotations for better type safety

--- src/api/main/account.ts
+++ src/api/main/account.ts
@@ -2,18 +2,23 @@ import { components } from 'types/ivy-types';
 
 import * as api from '../config';
 
+type Account = components['schemas']['Account'];
 type AccountBinding = components['schemas']['AccountBinding'];
-type Transaction = components['schemas']['Transaction'];
+type TransactionBinding = components['schemas']['TransactionBinding'];
+type TransactionPagedView = components['schemas']['TransactionPagedView'];
 
-const get = (filters?) => api.get('account', filters);
+const get = (filters?: { IsActive?: boolean }): Promise<Account[]> => api.get('account', filters);
 
-const getTransactions = (accountId: string, filters?) => api.get(`account/${accountId}/transaction`, filters);
+const getTransactions = (
+    accountId: string,
+    filters?: { PageAll?: boolean; Page?: number; PageSize?: number; From?: string; To?: string; OrderAscending?: boolean }
+): Promise<TransactionPagedView> => api.get(`account/${accountId}/transaction`, filters);
 
-const postTransaction = (accountId: string, data: Transaction) => api.post(`account/${accountId}/transaction`, data);
+const postTransaction = (accountId: string, data: TransactionBinding): Promise<number> => api.post(`account/${accountId}/transaction`, data);
 
-const post = (account: AccountBinding) => api.post('account', account);
+const post = (account: AccountBinding): Promise<number> => api.post('account', account);
 
-const put = (accountId: string, account: AccountBinding) => api.put(`account/${accountId}`, account);
+const put = (accountId: string, account: AccountBinding): Promise<number> => api.put(`account/${accountId}`, account);
 
 const account = {
     get,
